bfbf0ae9a28ab8c76a1f3fea05a98ed17243b74b
Nik0-9 <filannino.nicola9@gmail.com>
2024-03-27 15:13:03 +0100

    added var imag2 and secondTitle in data and print it in DOM using dynamic tag

--- js/myscript.js
+++ js/myscript.js
@@ -3,9 +3,12 @@ const { createApp } = Vue;
 createApp({
     data(){
         return{
-            title: 'Hello vue!',
+            title: 'Hello Vue!',
+            secondTitle: '<h2>this is h2 Hello Vue! </h2>',
             image1: 'img/vue.jpeg',
-            alt1: 'image vue'
+            alt1: 'image vue',
+            image2: '<img src="img/Vue2.webp" alt="image vue 2">',
+            alt2: 'imgae 2 vue'
         }
     },
     methods: {
